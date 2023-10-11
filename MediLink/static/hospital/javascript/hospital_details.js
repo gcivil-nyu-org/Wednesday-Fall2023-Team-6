46f7056e985218172fb766d59bf830c679b398a8
Chirangivi Bhat <32939338+Chinnu1103@users.noreply.github.com>
2023-10-11 17:56:39 -0400

    Changes for booking online and offline appointments (#37)

---
+++ MediLink/static/hospital/javascript/hospital_details.js
@@ -0,0 +1,53 @@
+$("#booking_form").submit(function(event) {
+    event.preventDefault();
+});
+
+document.getElementById("btn_confirm_details").onclick = function(url) {
+    var url = $("#btn_confirm_details").attr("data-url");
+
+    if(document.getElementById("date").value.trim() == "" ||
+        document.getElementById("time").value.trim() == "" ||
+        document.getElementById("name").value.trim() == "" ||
+        document.getElementById("phone").value.trim() == "" ||
+        document.getElementById("email").value.trim() == ""){
+        alert("Please fill the mandatory fields!");
+        return;
+    }
+
+    fetch(url, {
+        method: "POST",
+        credentials: 'same-origin',
+        body: JSON.stringify({
+            'date': document.getElementById("date").value,
+            'time': document.getElementById("time").value,
+            'name': document.getElementById("name").value,
+            'phone': document.getElementById("phone").value,
+            'email': document.getElementById("email").value,
+            'reason': document.getElementById("reason").value,
+            'accebility': document.getElementById("accessibility").value,       
+        }),
+    })
+    .then((response) => {
+        if(response.status == 200) {
+            return response.text();
+        }else {
+            throw new Error("Server Error: " + response.text());
+        }
+    })
+    .then((result) => {
+        document.getElementById("alert_message").textContent = result;
+        console.log(result);
+        $("#alert-container").show();
+
+        setTimeout(function() {
+            $("#alert-container").hide();
+        }, 5000);
+
+    })
+    .catch(error => {
+        // Handle any errors that occur during the fetch
+        alert("Error:" + error);
+    });
+
+    $("#book_appointment_modal").modal('hide');
+}
