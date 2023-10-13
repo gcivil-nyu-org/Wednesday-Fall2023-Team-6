154c636bfc7bb62245ee833f66de3db4802a863c
Jay <w742794749@gmail.com>
2023-10-12 23:06:31 -0400

    add view for login

--- MediLink/user/views.py
+++ MediLink/user/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+
+def LoginView(request):
+    template_name = 'user/login.html'
+    return render(request, template_name)
