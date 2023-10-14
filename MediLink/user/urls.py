034bf8b80dd12c3ba55bc61f51e9ba49e61b4e29
Jay <w742794749@gmail.com>
2023-10-14 13:01:09 -0400

    add reset password page

--- MediLink/user/urls.py
+++ MediLink/user/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'user'
 urlpatterns = [
     path('registration/', views.register, name='user_registration'),
-    path('login/', views.LoginView, name='login'),
+    path('login/', views.loginView, name='login'),
+    path('resetPassword/', views.restPwdView, name='resetPassword'),
 ]
