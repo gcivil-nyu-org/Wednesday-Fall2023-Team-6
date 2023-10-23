68b49960d6ba393787e6b2fabfb4c99d90c214be
Chirangivi Bhat <chirangivibhat@gmail.com>
2023-10-23 15:06:06 -0400

    DB Restructure

--- doctor/models.py
+++ doctor/models.py
@@ -16,9 +16,6 @@ class Doctor(User):
         "hospital.Hospital", max_length=100, null=True, on_delete=models.SET_NULL
     )
 
-    def __str__(self):
-        return self.first_name + " " + self.last_name
-
 
 class DoctorAppointment(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
