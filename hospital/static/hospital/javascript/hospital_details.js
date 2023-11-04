15e525d5b1589af1b86a6ad7ae8c720062418ed9
Chirangivi Bhat <chirangivibhat@gmail.com>
2023-11-04 17:25:16 -0400

    Hospital and Doctor List Changes

--- hospital/static/hospital/javascript/hospital_details.js
+++ hospital/static/hospital/javascript/hospital_details.js
@@ -4,6 +4,7 @@ $("#booking_form").submit(function(event) {
 
 document.getElementById("btn_confirm_details").onclick = function(url) {
     var url = $("#btn_confirm_details").attr("data-url");
+    var csrfToken = document.getElementsByName('csrfmiddlewaretoken')[0].value;
 
     if(document.getElementById("date").value.trim() == "" ||
         document.getElementById("time").value.trim() == "" ||
@@ -14,9 +15,26 @@ document.getElementById("btn_confirm_details").onclick = function(url) {
         return;
     }
 
+    doctor_id = null
+    if (document.getElementById("specialist").options.length > 0 && 
+        document.getElementById("specialist").value != "0"){
+            console.log("here")
+            doctor_id = document.getElementById("specialist").value
+    }
+    
+    date_str = `${document.getElementById("date").value}T${document.getElementById("time").value}`
+    given_date = new Date(date_str)
+    if(given_date < Date.now()){
+        alert("Please enter a valid date and time!")
+        return
+    }
+
     fetch(url, {
         method: "POST",
         credentials: 'same-origin',
+        headers: {
+            "X-CSRFToken": csrfToken,
+        },
         body: JSON.stringify({
             'date': document.getElementById("date").value,
             'time': document.getElementById("time").value,
@@ -24,29 +42,50 @@ document.getElementById("btn_confirm_details").onclick = function(url) {
             'phone': document.getElementById("phone").value,
             'email': document.getElementById("email").value,
             'reason': document.getElementById("reason").value,
-            'accebility': document.getElementById("accessibility").value,       
+            'preferred_doctor': doctor_id,
+            'accebility': document.getElementById("accessibility").value,
         }),
     })
-    .then((response) => {
-        if(response.status == 200) {
-            return response.text();
-        }else {
-            throw new Error("Server Error: " + response.text());
+    .then(response => Promise.all([response.status, response.text()]))
+    .then(([status, text_message]) => {
+        
+        if(status == 200){
+            removeClass = "alert-danger";
+            addClass = "alert-success";
+        }else{
+            removeClass = "alert-success";
+            addClass = "alert-danger"
+            if(status != 400) text_message = "Error: Could not create request. Please check your details and try again!"
         }
-    })
-    .then((result) => {
-        document.getElementById("alert_message").textContent = result;
-        console.log(result);
-        $("#alert-container").show();
 
-        setTimeout(function() {
-            $("#alert-container").hide();
-        }, 5000);
+        if(document.getElementById("alert-container").classList.contains(removeClass)){
+            console.log(document.getElementById("alert-container").classList);
+            document.getElementById("alert-container").classList.remove(removeClass)
+        }
+
+        if(!document.getElementById("alert-container").classList.contains(addClass))    
+            document.getElementById("alert-container").classList.add(addClass)
+
+        document.getElementById("alert_message").textContent = text_message;
+        $("#alert-container").show();
 
     })
     .catch(error => {
+
+        removeClass = "alert-success";
+        addClass = "alert-danger"
+        
+        if(document.getElementById("alert-container").classList.contains(removeClass)){
+            console.log(document.getElementById("alert-container").classList);
+            document.getElementById("alert-container").classList.remove(removeClass)
+        }
+
+        if(!document.getElementById("alert-container").classList.contains(addClass))    
+            document.getElementById("alert-container").classList.add(addClass)
+        
         // Handle any errors that occur during the fetch
-        alert("Error:" + error);
+        document.getElementById("alert_message").textContent = "Error: Could not create request. Please check your details and try again!";
+        $("#alert-container").show();
     });
 
     $("#book_appointment_modal").modal('hide');
--- hospital/views.py
+++ hospital/views.py
@@ -1,16 +1,16 @@
-from datetime import datetime
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
+from datetime import datetime, timedelta
+from django.core.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.views import generic
 from .models import Hospital, HospitalAppointment
-from user.models import User
+from user.models import Patient
 from doctor.models import Doctor
 from django.utils import timezone
-from django.views.decorators.clickjacking import xframe_options_exempt
-from django.views.decorators.csrf import csrf_exempt
 import json
 from django.core.paginator import Paginator
 from .forms import HospitalFilterForm
+from django.db.models import Q
 
 
 class HospitalDetailView(generic.DetailView):
@@ -20,28 +20,12 @@ class HospitalDetailView(generic.DetailView):
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
 
-
-def TempDetails(request):
-    template_name = "hospital/hospital_details.html"
-    return render(request, template_name)
-
-
-def get_hospitals(request):
-    # Query all hospitals from the database
-    hospitals = Hospital.objects.all()
-
-    # Convert queryset to a list of dictionaries
-    hospital_list = [
-        {
-            "name": hospital.name,
-            "facility_type": hospital.facility_type,
-            # Add other fields as needed
-        }
-        for hospital in hospitals
-    ]
-
-    # Return the list as JSON
-    return JsonResponse({"hospitals": hospital_list})
+    def get_context_data(self, **kwargs):
+        context = super(HospitalDetailView, self).get_context_data(**kwargs)
+        context["doctors"] = Doctor.objects.all().filter(
+            associated_hospital=context["object"]
+        )
+        return context
 
 
 class HospitalListView(generic.ListView):
@@ -113,41 +97,86 @@ class HospitalListView(generic.ListView):
         return context
 
 
-@xframe_options_exempt
-@csrf_exempt
-def book_appointment(request, hospital_id):
-    hospital = get_object_or_404(Hospital, pk=hospital_id)
-    try:
-        body = json.load(request)
-        user_id = 1
-        start_time = datetime.strptime(
-            f'{body["date"]} {body["time"]}', "%Y-%m-%d %H:%M"
-        )
-        start_time = timezone.make_aware(start_time)
-        preferred_doctor = get_object_or_404(Doctor, pk=1)
-        name = body["name"]
-        phone = body["phone"]
-        email = body["email"]
-        reason = body["reason"]
-        accebility = body["accebility"]
+def check_appointment_overlap(patient, appointment_dtime):
+    end_time = appointment_dtime + timedelta(minutes=30)
 
-    except Exception as e:
-        print("Error: ", e)
-        return HttpResponseBadRequest("Invalid Request")
+    start_check = Q(start_time__lte=end_time)
+    end_check = Q(start_time__gte=(appointment_dtime - timedelta(minutes=30)))
+    overlapping_appointments = HospitalAppointment.objects.filter(
+        start_check & end_check,
+        patient=patient,
+    )
 
+    if overlapping_appointments.exists():
+        return (True, "You have an overlapping appointment/request at that time")
     else:
-        appointment = HospitalAppointment()
-        appointment.user = get_object_or_404(User, pk=user_id)
-        appointment.hospital = hospital
-        appointment.name = name
-        appointment.phone = phone
-        appointment.email = email
-        appointment.reason = reason
-        appointment.accebility = accebility
-        appointment.start_time = start_time
-        appointment.preferred_doctor = preferred_doctor
-        appointment.status = "REQ"
-        appointment.save()
-        print("Appointment Saved")
+        return (False, "")
 
-        return HttpResponse("Appointment Request Created Successfully!", status=200)
+
+def book_appointment(request, hospital_id):
+    if request.method == "POST":
+        if not request.user.is_authenticated:
+            return HttpResponseBadRequest(
+                "Cannot create an appointment without an account. Please Login!"
+            )
+
+        hospital = get_object_or_404(Hospital, pk=hospital_id)
+        try:
+            body = json.load(request)
+            if Patient.objects.all().filter(email=request.user.email).exists():
+                user = get_object_or_404(Patient, email=request.user.email)
+            else:
+                return HttpResponseBadRequest(
+                    "You need to have a Patient account to create appointments!"
+                )
+
+            start_time = datetime.strptime(
+                f'{body["date"]} {body["time"]}', "%Y-%m-%d %H:%M"
+            )
+            start_time = timezone.make_aware(start_time)
+
+            overlap, overlap_message = check_appointment_overlap(user, start_time)
+            if overlap:
+                return HttpResponseBadRequest(overlap_message)
+
+            preferred_doctor = None
+            print(body["preferred_doctor"])
+            if body["preferred_doctor"]:
+                preferred_doctor = get_object_or_404(
+                    Doctor, pk=int(body["preferred_doctor"])
+                )
+
+            name = body["name"]
+            phone = body["phone"]
+            email = body["email"]
+            reason = body["reason"]
+            accebility = body["accebility"]
+
+        except Exception as e:
+            print("Error: ", e)
+            return HttpResponseBadRequest("Invalid Request")
+
+        try:
+            appointment = HospitalAppointment()
+            appointment.patient = user
+            appointment.hospital = hospital
+            appointment.name = name
+            appointment.phone = phone
+            appointment.email = email
+            appointment.reason = reason
+            appointment.accebility = accebility
+            appointment.start_time = start_time
+            appointment.preferred_doctor = preferred_doctor
+            appointment.status = "REQ"
+
+            appointment.full_clean()
+            appointment.save()
+            print("Appointment Saved")
+            return HttpResponse("Appointment Request Created Successfully!", status=200)
+        except ValidationError as ve:
+            print("Validation Error:", ve)
+            return HttpResponseBadRequest(
+                "Validation Error! Please ensure your details are correct"
+            )
+
+    return HttpResponseBadRequest("Invalid Request Method")
