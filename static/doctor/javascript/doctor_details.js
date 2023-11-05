d91323ba46f0f746807b798036cffe0970488cea
Chirangivi Bhat <chirangivibhat@gmail.com>
2023-11-04 21:07:22 -0400

    Navbar Navigation

--- static/doctor/javascript/doctor_details.js
+++ static/doctor/javascript/doctor_details.js
@@ -5,6 +5,7 @@ $("#booking_form").submit(function(event) {
 
 document.getElementById("btn_confirm_details").onclick = function(url) {
     var url = $("#btn_confirm_details").attr("data-url");
+    var csrfToken = document.getElementsByName('csrfmiddlewaretoken')[0].value;
 
     if(document.getElementById("date").value.trim() == "" ||
         document.getElementById("time").value.trim() == "" ||
@@ -15,9 +16,19 @@ document.getElementById("btn_confirm_details").onclick = function(url) {
         return;
     }
 
+    date_str = `${document.getElementById("date").value}T${document.getElementById("time").value}`
+    given_date = new Date(date_str)
+    if(given_date < Date.now()){
+        alert("Please enter a valid date and time!")
+        return
+    }
+
     fetch(url, {
         method: "POST",
         credentials: 'same-origin',
+        headers: {
+            "X-CSRFToken": csrfToken,
+        },
         body: JSON.stringify({
             'date': document.getElementById("date").value,
             'time': document.getElementById("time").value,
@@ -27,26 +38,46 @@ document.getElementById("btn_confirm_details").onclick = function(url) {
             'reason': document.getElementById("reason").value,
         }),
     })
-    .then((response) => {
-        if(response.status == 200) {
-            return response.text();
-        }else {
-            throw new Error("Server Error: " + response.text());
+    .then(response => Promise.all([response.status, response.text()]))
+    .then(([status, text_message]) => {
+        
+        if(status == 200){
+            removeClass = "alert-danger";
+            addClass = "alert-success";
+        }else{
+            removeClass = "alert-success";
+            addClass = "alert-danger"
+            if(status != 400) text_message = "Error: Could not create request. Please check your details and try again!"
         }
-    })
-    .then((result) => {
-        document.getElementById("alert_message").textContent = result;
-        console.log(result);
-        $("#alert-container").show();
 
-        setTimeout(function() {
-            $("#alert-container").hide();
-        }, 5000);
+        if(document.getElementById("alert-container").classList.contains(removeClass)){
+            console.log(document.getElementById("alert-container").classList);
+            document.getElementById("alert-container").classList.remove(removeClass)
+        }
+
+        if(!document.getElementById("alert-container").classList.contains(addClass))    
+            document.getElementById("alert-container").classList.add(addClass)
+
+        document.getElementById("alert_message").textContent = text_message;
+        $("#alert-container").show();
 
     })
     .catch(error => {
+
+        removeClass = "alert-success";
+        addClass = "alert-danger"
+        
+        if(document.getElementById("alert-container").classList.contains(removeClass)){
+            console.log(document.getElementById("alert-container").classList);
+            document.getElementById("alert-container").classList.remove(removeClass)
+        }
+
+        if(!document.getElementById("alert-container").classList.contains(addClass))    
+            document.getElementById("alert-container").classList.add(addClass)
+
         // Handle any errors that occur during the fetch
-        alert("Error:" + error);
+        document.getElementById("alert_message").textContent = "Error: Could not create request. Please check your details and try again!";
+        $("#alert-container").show();
     });
 
     $("#book_appointment_modal").modal('hide');
