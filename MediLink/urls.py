cc51a5922945262f34844b3cde53e852d7e02e6c
Chirangivi Bhat <32939338+Chinnu1103@users.noreply.github.com>
2023-11-05 20:22:01 -0500

    Navbar Navigation and Contact Popup
    * Navbar Navigation
    
    * Contact Popup

--- MediLink/urls.py
+++ MediLink/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path("", lambda req: redirect("/user/home")),
     path("admin/", admin.site.urls),
     path("user/", include("user.urls")),
     path("doctor/", include("doctor.urls")),
