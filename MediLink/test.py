4af133ddc5458f2646639b6cb082cc52dae4d7bc
Jay <w742794749@gmail.com>
2023-11-09 16:43:04 -0500

    test cases for asgi and wsgi

---
+++ MediLink/test.py
@@ -0,0 +1,17 @@
+import unittest
+import os
+
+class TestASGIConfig(unittest.TestCase):
+    def test_asgi_config_syntax(self):
+        asgi_file_path = os.path.join(os.path.dirname(__file__), 'asgi.py')
+        with open(asgi_file_path, 'rb') as asgi_file:
+            compile(asgi_file.read(), asgi_file_path, 'exec')
+
+class TestWSGIConfig(unittest.TestCase):
+    def test_wsgi_config_syntax(self):
+        wsgi_file_path = os.path.join(os.path.dirname(__file__), 'wsgi.py')
+        with open(wsgi_file_path, 'rb') as wsgi_file:
+            compile(wsgi_file.read(), wsgi_file_path, 'exec')
+
+if __name__ == '__main__':
+    unittest.main()
