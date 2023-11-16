42dab6dcebeb2141f663b63fbccb783a86d47db2
Hao-191 <Hao-191@users.noreply.github.com>
2023-11-16 03:42:09 +0000

    Autoformat code with Black

--- googleMaps/urls.py
+++ googleMaps/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('doctorMap/', views.doctor_map, name='doctor_map'),
+    path("doctorMap/", views.doctor_map, name="doctor_map"),
 ]
--- googleMaps/views.py
+++ googleMaps/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def doctor_map(request):
     template_name = "googleMaps/doctorMap.html"
     lat, lon = 40.7167, -74.0000
-    return render(request, template_name, {'latitude': lat, 'longitude': lon})
+    return render(request, template_name, {"latitude": lat, "longitude": lon})
