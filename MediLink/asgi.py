5e617e30067fefba9e45c5a8c57a09d5b65d120a
Chirangivi Bhat <chirangivibhat@gmail.com>
2023-11-26 17:50:16 -0500

    temp

--- MediLink/asgi.py
+++ MediLink/asgi.py
@@ -8,17 +8,17 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MediLink.settings")
+django.setup()
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+django_asgi_app = get_asgi_application()
 import chat.routing
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MediLink.settings")
 
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
-
 
 application = ProtocolTypeRouter(
     {
