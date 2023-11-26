8349abb47bc947b255dfddb04565d1ac3e0ad91b
Chirangivi Bhat <chirangivibhat@gmail.com>
2023-11-26 17:56:18 -0500

    temp

--- MediLink/settings.py
+++ MediLink/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "ebhealthcheck.apps.EBHealthCheckConfig",
     "hospital",
     "user",
     "doctor",
