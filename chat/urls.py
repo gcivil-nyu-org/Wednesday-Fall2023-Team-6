3b501d03fe0c3ec7c32c1a8affe30d20699f0a4f
Sashank rm <sashankrm11@gmail.com>
2023-11-28 00:57:49 -0500

    Fixx (#218)

--- chat/urls.py
+++ chat/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "chat"
 
 urlpatterns = [
-    # ... other url patterns ....
+    # ... other url patterns .....
     path(
         "<int:appointment_id>/", views.chat, name="chat"
     ),  # The 'name' argument should match your template tag.
