9df807cd4705c28afbe8051eeffe1fbfb74e8264
Jay <w742794749@gmail.com>
2023-11-29 20:40:29 -0500

    chat test cases 1

--- chat/tests/tests_chats.py
+++ chat/tests/tests_chats.py
@@ -12,6 +12,7 @@ from django.contrib.auth.models import User
 
 from chat.models import Message
 from doctor.models import Doctor, DoctorAppointment
+from hospital.models import Hospital, HospitalAdmin
 from user.models import Patient
 
 
@@ -25,6 +26,7 @@ class BaseChatViewTest(TestCase):
             password="testpassword",
             email="testuser@example.com",
         )
+
         self.patient = Patient.objects.create(
             email="testuser@example.com",
             name="Test User",
@@ -41,6 +43,34 @@ class BaseChatViewTest(TestCase):
             email="testdoctor@example.com",
             name="Test Doctor",
             zip="12345",
+        )
+
+        # Create an hospitalAdmin user
+        self.hospital = Hospital.objects.create(
+            name="Test Hospital",
+            facility_type="General",
+            borough="MHT",
+            phone="1112223333",
+            location="789 Street",
+            postal_code=67890,
+            latitude=40.7128,
+            longitude=-74.0060,
+        )
+
+        self.hospital_admin_user = User.objects.create_user(
+            username="testadmin@example.com",
+            password="testpassword",
+            email="testadmin@example.com",
+        )
+
+        self.hospital_admin = HospitalAdmin.objects.create(
+            email="testadmin@example.com",
+            name="Test Admin",
+            phone="1234567890",
+            address="123 Street",
+            borough="MHT",
+            zip="54321",
+            associated_hospital=self.hospital,
         )
 
         # Create an appointment
@@ -123,11 +153,97 @@ class TestPostAttachment(BaseChatViewTest):
         message.attachment.delete()  # Clean up
 
 
-class TestUnauthorizedUserAccess(BaseChatViewTest):
-    def test_unauthorized_user_access(self):
+class TestLogoutUserAccess(BaseChatViewTest):
+    def test_logout_user_access(self):
         self.client.logout()
         response = self.client.get(reverse("chat:chat", args=(self.appointment.id,)))
         expected_redirect_url = f"{reverse('user:login')}?next={reverse('chat:chat', args=(self.appointment.id,))}"
         self.assertRedirects(
             response, expected_redirect_url, status_code=302, target_status_code=200
         )
+
+
+class TestDoctorUserAccess(BaseChatViewTest):
+    def test_doctor_user_access(self):
+        self.client.login(username="testdoctor@example.com", password="testpassword")
+        response = self.client.get(reverse("chat:chat", args=(self.appointment.id,)))
+        self.assertEqual(response.status_code, 302)
+
+
+class TestHospitalAdminAccess(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        # Create a patient user
+        self.user = User.objects.create_user(
+            username="testuser@example.com",
+            password="testpassword",
+            email="testuser@example.com",
+        )
+
+        self.patient = Patient.objects.create(
+            email="testuser@example.com",
+            name="Test User",
+            zip="54321",
+        )
+
+        # Create a doctor user
+        self.doctor_user = User.objects.create_user(
+            username="testdoctor@example.com",
+            password="testpassword",
+            email="testdoctor@example.com",
+        )
+        self.doctor = Doctor.objects.create(
+            email="testdoctor@example.com",
+            name="Test Doctor",
+            zip="12345",
+        )
+
+        # Create an hospitalAdmin user
+        self.hospital = Hospital.objects.create(
+            name="Test Hospital",
+            facility_type="General",
+            borough="MHT",
+            phone="1112223333",
+            location="789 Street",
+            postal_code=67890,
+            latitude=40.7128,
+            longitude=-74.0060,
+        )
+
+        self.hospital_admin_user = User.objects.create_user(
+            username="testadmin@example.com",
+            password="testpassword",
+            email="testadmin@example.com",
+        )
+
+        self.hospital_admin = HospitalAdmin.objects.create(
+            email="testadmin@example.com",
+            name="Test Admin",
+            phone="1234567890",
+            address="123 Street",
+            borough="MHT",
+            zip="54321",
+            associated_hospital=self.hospital,
+        )
+
+        # Create an appointment
+        self.appointment = DoctorAppointment.objects.create(
+            doctor=self.doctor,
+            patient=self.patient,
+            status="CNF",  # Pending status
+            start_time=datetime.datetime.now(),
+        )
+
+        # Temporary media root for file uploads
+        self.temp_media_root = tempfile.mkdtemp()
+
+    def test_hospitalAdmin_access(self):
+        self.client.login(username="testadmin@example.com", password="testpassword")
+        response = self.client.get(reverse("chat:chat", args=(self.appointment.id,)))
+        self.assertRedirects(
+            response, reverse("user:account"), status_code=302, target_status_code=200
+        )
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), "Unauthorized User")
