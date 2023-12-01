f083205051297c6361833a321b02fc27c302ef87
Sashank rm <sashankrm11@gmail.com>
2023-12-01 11:31:27 -0500

    Migrate postgres (#181)

--- MediLink/settings.py
+++ MediLink/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from django.contrib.messages import constants as messages
 import os
+import sys
 from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -99,14 +100,24 @@ ASGI_APPLICATION = "MediLink.asgi.application"
 
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", "medilink_database"),
+        "USER": os.environ.get("DB_USER", "medilink"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "abc123xyz"),
+        "HOST": os.environ.get("DB_HOST", "127.0.0.1"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
+
+if "test" in sys.argv:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": ":memory:",
+        }
+    }
 
 
 # Password validation
